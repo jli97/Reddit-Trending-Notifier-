e1974d05d399a222f0f7ca65a17f54f8f88253cc
Jonathan Li <jonathan.li.1997@gmail.com>
2020-05-22 12:47:22 -0400

    Completed Functionality of Script

--- src/getData.py
+++ src/getData.py
@@ -3,29 +3,17 @@ import math
 import time
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+
 class Data:
-    reddit = praw.Reddit(client_id = 'aM_mGMSXor7avg',client_secret = '	rlfl7DoUGE74y_5UlcP2jLntoVw', username='PRAW_97', password='123456789j', user_agent='Better Trending')
+    reddit = praw.Reddit(client_id = 'aM_mGMSXor7avg',client_secret = 'rlfl7DoUGE74y_5UlcP2jLntoVw', username='PRAW_97', password='123456789j', user_agent='Better Trending')
     
     def __init__(self, sub_name):
         self.sub_name = sub_name
         self.subreddit = self.reddit.subreddit(sub_name)
-        self.new_posts = self.getNewPosts() # Only look at posts posted within 24hrs
-        self.model = self.regression() 
+        self.new_posts = self.getNewPosts() # Posts within 24 hours
+        self.data_frame = self.getRegressionDf()
+        self.lr_obj = self.regression()
 
-    def getSTD(sub_name):
-    mean = getAverageUpVotes(sub_name)
-
-    sum = 0
-    count = 0
-    for submission in subreddit.hot(limit=50):
-        sum = pow((sum + (submission.score - mean)), 2)
-        count = count + 1
-
-    sum = sum/(count - 1)
-    std = sqrt(sum)
-
-    return std
 
     def getNewPosts(self):
         ret = []
@@ -33,28 +21,37 @@ class Data:
             date_created = submission.created_utc # Unix Time
             date_today = time.time() 
 
-            if((date_today - date_created) > (24*60*60)): # Post is less than 24hrs old
-                ret.insert(submission)
+            if((date_today - date_created) < (24*60*60) and not (submission.stickied)): # Post is less than 24hrs old
+                ret.append(submission)
+
+        return ret
+    
+    def getRegressionDf(self): # Returns dataframe
+        #Get Data
+        upvotes = [] 
+        post_age = [] 
+
+        for submission in self.subreddit.hot(limit=25): #First page
+            if submission.stickied:
+                continue
+            upvotes.append(submission.score)
+            post_age.append(time.time() - submission.created_utc)
         
+        independent = {'post_age': post_age}
+        ret = pd.DataFrame(data=independent)
+        ret['upvotes'] = upvotes
         return ret
     
     def regression(self): # Returns LinearRegression Object with score as dependent and num_comment, pos_age as independent
-        #Get Data
-        upvotes = [] 
-        num_comment = [] 
-        post_age = [] 
 
-        for submission in self.subreddit.hot(limit=50):
-            upvotes.insert(submission.score)
-            num_comment.insert(submission.num_comments)
-            post_age.insert(submission.created_utc - time.time())
-        
-        independent = {'num_comment': num_comment, 'post_age': post_age}
-        dependent = {'upvotes': upvotes}
-        x = pd.DataFrame(data=independent)
-        y = pd.DataFrame(data=dependent)
+        x = self.data_frame.iloc[:,0].copy()
+        y = self.data_frame.iloc[:,1].copy()
+
+        x = x.values.reshape(-1,1)
+        y= y.values.reshape(-1,1)
         #Regression
-        lr = LinearRegression(fit_intercept=False)
+        lr = LinearRegression(fit_intercept=False) # If post age is 0, upvotes should be 0
         lr.fit(x,y)
 
         return lr
+
--- src/main.py
+++ src/main.py
@@ -1,14 +1,53 @@
 import getData
-import datetime
+import time
+from sklearn.metrics import mean_squared_error
+from scipy.stats import norm
+import numpy as np
+#gkm73p
+
 
 def main():
-    name = "FrugalMaleFashionCDN"
-    sub_data = getData.Data(name)
+    sub_name = "FrugalMaleFashionCDN"
+    sub_data = getData.Data(sub_name)
 
-    for submission in sub_data.new_posts: # Submissions that are less than 24hrs old
+
+    for submission in sub_data.new_posts: # Only checks posts created within 24hours
+        print(submission.title)
         if(isTrending(sub_data, submission)):
-            #Send message
+            msg = "This [post]("+submission.permalink+ ") is trending on /r/ " + sub_name
+            #sub_data.reddit.redditor('jonyface').message("Trend Notification",msg)
+        
 
 def isTrending(sub_data, submission):
-    
+    x = sub_data.data_frame.iloc[:,0].copy()
+    x = x.values.reshape(-1,1) 
+    y_actual = sub_data.data_frame.iloc[:,1].copy()
+    y_actual = y_actual.values.reshape(-1,1)
 
+    pred = sub_data.lr_obj.coef_ * (time.time() - submission.created_utc)
+    y_model = sub_data.lr_obj.predict(x)
+    
+    upper_pred = getUpperPrediction(pred,y_actual,y_model,0.95)
+    
+    if (submission.score > upper_pred):
+        return True
+    else:
+        return False
+
+
+def getUpperPrediction(prediction, y_actual, y_model, pi):
+    #stdev of y_actual
+    sum_errs = np.sum((y_actual - y_model)**2)
+    stdev = np.sqrt(1 / (len(y_actual) - 2) * sum_errs)
+
+    #Interval
+    prob_value = 1 - ((1 - pi) / 2)
+    z_score = norm.ppf(prob_value)
+    interval = z_score * stdev
+
+    #generate prediction interval lower and upper bound
+    ret = prediction + interval
+    return ret
+
+if __name__ == "__main__":
+    main()
