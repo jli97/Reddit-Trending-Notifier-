5b63afeca799c4fde823caab81baad3a68e57baf
Jonathan Li <jonathan.li.1997@gmail.com>
2020-05-27 17:38:16 -0400

    Removed old files

--- src/test.py
+++
@@ -1,5 +0,0 @@
-import praw
-
-reddit = praw.Reddit(client_id = 'aM_mGMSXor7avg',client_secret = 'rlfl7DoUGE74y_5UlcP2jLntoVw', username='PRAW_97', password='123456789j', user_agent='Better Trending')
-
-print(reddit.subreddit("pics"))
